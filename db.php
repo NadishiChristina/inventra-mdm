01b795a66b6b9f1ba64e280fb87516d7f4f2804c
NadishiChristina <nadishiramanayake@gmail.com>
2025-05-05 23:50:29 +0530

    Added DB connection

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'mdm_db';
+$user = 'root';
+$pass = '';
+$conn = mysqli_connect($host, $user, $pass, $db, 3307);
+//if the DB connection fails, display an error message and exit
+if (!$conn)
+{
+die('Could not connect: ' . mysqli_error($conn));
+}
+?>
