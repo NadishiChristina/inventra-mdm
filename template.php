76f3baee860e40e55d87978c18baa03d3f6027e0
NadishiChristina <nadishiramanayake@gmail.com>
2025-05-06 14:47:49 +0530

    Modified template file with redirect code

--- template.php
+++ template.php
@@ -1,12 +1,24 @@
 <?php
-$pagename="template"; 
+session_start();
+include("db.php");
+
+// Redirect if not logged in
+if (!isset($_SESSION['id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$pagename="template"; //Create and populate a variable called $pagename
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in stylesheet
 echo "<title>".$pagename."</title>"; //display name of the page as window title
 echo "<body>";
-include ("headerfile.html"); //include header layout 
-echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-//display random text
+include("headfile.html"); // include header layout file
+include("detectlogin.php"); // display the logged-in username across pages
+echo "<h4>".$pagename."</h4>"; // display name of the page on the web page
+
+//content
 echo "<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
-include("footerfile.html"); //include footer layout
+
+include("footfile.html"); //include footer layout
 echo "</body>";
 ?>
