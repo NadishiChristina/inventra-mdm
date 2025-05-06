53a46a9bde68e7b4e9175e81a9e979607969467b
NadishiChristina <nadishiramanayake@gmail.com>
2025-05-06 14:46:31 +0530

    Modified db connection file

--- db.php
+++ db.php
@@ -4,9 +4,9 @@ $db = 'mdm_db';
 $user = 'root';
 $pass = '';
 $conn = mysqli_connect($host, $user, $pass, $db, 3307);
+
 //if the DB connection fails, display an error message and exit
-if (!$conn)
-{
+if (!$conn){
 die('Could not connect: ' . mysqli_error($conn));
 }
 ?>
